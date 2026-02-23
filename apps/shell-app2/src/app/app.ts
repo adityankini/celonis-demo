f42c2c06aa6bfac3b05296998078c0e8da8962e9
Aditya Kini <adityakini@Adityas-MacBook-Pro.local>
2026-02-23 20:21:29 +0530

    Temp changes

---
+++ apps/shell-app2/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import { SharedDataAccess } from '@celonis-demo/shared-data-access';
+
+@Component({
+  standalone: true,
+  // imports: [SharedDataAccess],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {}
